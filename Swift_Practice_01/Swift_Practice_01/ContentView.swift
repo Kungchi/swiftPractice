84ca06888e02e55af68161150ed0be17ab6f778e
KSH <a01027756032@gmail.com>
2023-01-08 13:44:19 +0900

    2023/01/08

---
+++ Swift_Practice_01/Swift_Practice_01/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Swift_Practice_01
+//
+//  Created by 김상훈 on 2023/01/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Practice Test 01")
+                .font(.title)
+                .foregroundColor(Color.pink)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }.padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift_Practice_01/Swift_Practice_01/Swift_Practice_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_01App.swift
+//  Swift_Practice_01
+//
+//  Created by 김상훈 on 2023/01/07.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
